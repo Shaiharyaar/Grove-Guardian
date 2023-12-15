bf563edd2e94087fda37738e7be2f4e939c3d1e4
Shaiharyaar <shaiharyaar.ahmad@onestream.live>
2023-12-15 22:50:09 +0200

    adjustment

--- src/modules/common/Header/index.js
+++ src/modules/common/Header/index.js
@@ -64,7 +64,7 @@ export default function Header() {
           fontWeight: 500,
           marginRight: 100,
         }}
-        href='/faq'
+        href='/login'
       >
         Login
       </a>
